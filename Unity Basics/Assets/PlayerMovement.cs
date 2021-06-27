09cf10b2dfb9f608618203b60c36426aab23fba3
cmpro <68463183+cmpro-bot@users.noreply.github.com>
2021-06-27 17:41:58 +0300

    ok

--- Unity Basics/Assets/PlayerMovement.cs
+++ Unity Basics/Assets/PlayerMovement.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;    
+    public float forwardForce = 1000f;
+    public float sideForce = 1000f;
+    public float upForce = 1500f;
     void Start()
     {
-        
+       
     }
 
  
     void FixedUpdate()
     {
-        rb.AddForce(0,0,2000* Time.deltaTime);
+        if(Input.GetKey("w"))
+        {
+             rb.AddForce(0,0,forwardForce* Time.deltaTime);
+        }
+        if(Input.GetKey("s"))
+        {
+             rb.AddForce(0,0,-forwardForce* Time.deltaTime);
+        }
+       if(Input.GetKey("d"))
+        {
+             rb.AddForce(sideForce* Time.deltaTime,0,0);
+        }
+        if(Input.GetKey("a"))
+        {
+             rb.AddForce(-sideForce* Time.deltaTime,0,0);
+        }
+         if(Input.GetKey("e"))
+        {
+             rb.AddForce(0,upForce* Time.deltaTime,0);
+        }
     }
 }
