7021384ae30fa121b8ba129497866a4c817d6cfb
cmpro <68463183+cmpro-bot@users.noreply.github.com>
2021-06-28 14:02:10 +0300

    fixed jump, added camera tracking

--- Unity Basics/Assets/Scripts/PlayerMovement.cs
+++ Unity Basics/Assets/Scripts/PlayerMovement.cs
@@ -3,16 +3,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody rb;    
-    public float forwardForce = 1000f;
-    public float sideForce = 1000f;
-    public float upForce = 5f;
-    void Start()
-    {
-       
-    }
-
- 
+     public Rigidbody rb;    
+     public float forwardForce = 1000f;
+     public float sideForce = 1000f;
+     public float upForce = 5f;
+     Vector3 e;
+     void Start()
+     {
+          e = new Vector3(0.0f,1.0f,0.0f);   
+     }
+     
     void FixedUpdate()
     {
         if(Input.GetKey("w"))
@@ -25,15 +25,14 @@ public class PlayerMovement : MonoBehaviour
         }
        if(Input.GetKey("d"))
         {
-             rb.AddForce(sideForce* Time.deltaTime,0,0);
+            rb.AddForce(sideForce* Time.deltaTime,0,0);
         }
         if(Input.GetKey("a"))
         {
-             rb.AddForce(-sideForce* Time.deltaTime,0,0);
-        }
+            rb.AddForce(-sideForce* Time.deltaTime,0,0);
+     }
          if(Input.GetKeyDown(KeyCode.Space))
         {
-             rb.AddForce(new Vector3(0, upForce, 0), ForceMode.Impulse);
-        }
+            rb.velocity = e* upForce;
     }
-}
+}}
